fae0335b2ce5b4058840ec2081797004c00161a6
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2021-12-10 20:20:03 +0100

    Cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export PHPRC=$(dirname "$0")
 composer global require clue/phar-composer
-rm $(basename $PWD)
 composer install --prefer-dist
+rm gh-prcr
 composer global exec phar-composer build $PWD $(basename $PWD)
